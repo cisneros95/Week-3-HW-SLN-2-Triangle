d42d6147ee32e85ae9b02715bdb8ebcec4fe15ea
Raul Cisneros <raul.f.cisneros@outlook.com>
2020-05-24 20:09:17 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week_3_HW_SLN_2_Triangle
+{
+    class Program
+    {
+
+        // Function to  calculate for validity
+        public static int checkValidity(int a, int b, int c)
+
+        {
+
+            // check condition
+            if (a + b <= c || a + c <= b || b + c <= a)
+                return 0;
+            else
+                return 1;
+
+        
+        
+        }
+
+        //Driver Code
+        static void Main()
+        {
+            int a = 7, b = 10, c = 5;
+
+            // function calling and print out
+            if ((checkValidity(a, b, c)) == 1)
+                Console.WriteLine("True");
+            else
+                Console.WriteLine("False");
+            
+
+        }
+    }
+}
